11ef69c941108865eedbcb5e2f3aba3263234010
Sebastian Cuneo <sebastian.cuneo.s1@gmail.com>
2025-03-28 10:30:02 -0300

    Parte 2 de 2 contador

--- TP3/src/Paquetito/ContadorPalabras.java
+++ TP3/src/Paquetito/ContadorPalabras.java
@@ -2,9 +2,14 @@ package Paquetito;
 
 public class ContadorPalabras {
 	
-	public int ContarPalabras(String frase) {
+	public int ContarPalabras(String frase, int xCaracteres) {
 		
+		int cantidadVocales = 0;
+		int cantidadConsonantes = 0;
 		int contador = 1;
+		int palabrasLargas = 0;
+		int cantidadLetras = 0;
+		int largoPalabra = 0;
 			
 		if (frase.length() > 0 && frase.charAt(0) == ' ') {
 		    frase = frase.substring(1);
@@ -14,12 +19,50 @@ public class ContadorPalabras {
 		    frase = frase.substring(0, frase.length() - 1);
 		}
 		
+        frase = frase.toLowerCase();
+        
 		for(int i = 0; i < frase.length(); i++) {
+			char c = frase.charAt(i);
+			
 			if(frase.charAt(i)== ' ') {
 				contador++;
+				
+				// Al cerrar una palabra, evaluamos si era larga
+				if(largoPalabra > xCaracteres) {
+					palabrasLargas++;
+				}
+				largoPalabra = 0; // reiniciar para la próxima palabra
+
+			}else if (Character.isLetter(c)) {
+				if(esVocal(c)) {
+					cantidadVocales ++;
+				}else {
+					cantidadConsonantes ++;
+				}
+				largoPalabra++;
 			}
 		}
+		// Evaluar última palabra (por si no terminó con espacio)
+				if(largoPalabra > xCaracteres) {
+					palabrasLargas++;
+				}
+		///////////////////////////////////////////////////////
+		
+		System.out.println("Total vocales: " + cantidadVocales);
+	    System.out.println("Total consonantes: " + cantidadConsonantes);
+	    System.out.println("Total palabras: " + contador);
+//	    if(xCaracteres < cantidadLetras) {
+//	    	palabrasLargas ++;
+//	    }
+	    System.out.println("Cantidad palabras largas: " + palabrasLargas);
 		return contador;
 	}
 	
+	private boolean esVocal(char c) {
+		if(c == 'a'|| c== 'e'||c == 'i' || c == 'o' || c == 'u' ) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 }
--- TP3/src/Paquetito/Principal.java
+++ TP3/src/Paquetito/Principal.java
@@ -7,10 +7,10 @@ public class Principal {
 		 ContadorPalabras cp = new ContadorPalabras();
 
 	        String frase1 = "Hola mundo";
-	        System.out.println("Frase 1: " + cp.ContarPalabras(frase1) + " palabras"); // 2
+	        System.out.println("Frase 1: " + cp.ContarPalabras(frase1, 8) + " palabras"); // 2
 	        
-	        String frase2 = "Hola mu ndo ";
-	        System.out.println("Frase 2: " + cp.ContarPalabras(frase2) + " palabras"); // 3
+	        String frase2 = "Hola mu ndoo ";
+	        System.out.println("Frase 2: " + cp.ContarPalabras(frase2, 3) + " palabras"); // 3
 	}
 
 }
